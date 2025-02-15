80738e91912c82b91d0cdd9f33283e52a6d74962
Marin <m-menkshi@hvcc.edu>
2025-02-15 14:42:35 -0500

    fixed secret

--- drawnBackEnd/src/main/java/com/casino/drawn/Services/JWT/JwtUtil.java
+++ drawnBackEnd/src/main/java/com/casino/drawn/Services/JWT/JwtUtil.java
@@ -5,14 +5,15 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-
-    private static final String SECRET = "0vwaZQMxY8qJubVDcASrxWSRkgVmAnWT";
+    @Value("${JWT_SECRET}")
+    private String SECRET;
     private static final long EXPIRATION_TIME = 86_400_000;
     private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
